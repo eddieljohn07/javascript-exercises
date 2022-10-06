d49365c0742e1d34ab24235bbd22ac97bc91061c
Eddiel John Velasco <darkvelasco2001@gmail.com>
2022-10-07 04:41:55 +0800

    follow instructions in readme

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(_repeat, _times) {
+    let n = 0
+    while (n === 0){
+        _times = 0
+        for (let i= 0; _times > i; _times++)
+        if (_times > 0){
+            _word = 'hey'
+            let _repeat = _word.repeat(_times);
+            n++;
+            return _repeat;
+        }else {
+            return 'ERROR'
+        }
+        }
+    }
 // Do not edit below this line
 module.exports = repeatString;
