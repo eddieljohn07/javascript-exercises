961a3932f9334cbc489a1705eab0470376a4d138
Eddiel John Velasco <darkvelasco2001@gmail.com>
2022-10-10 03:28:06 +0800

    string to array then reversed then join as string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(_reversedString) {
+    let string = 'hello there';
+    let stringArray = string.split("");
+    let backward = stringArray.reverse();
+    _reversedString = backward.join('')
+    return _reversedString
+}
+console.log(reverseString());
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString; 
